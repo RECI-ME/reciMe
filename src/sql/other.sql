b687a8375eaf1e8c2718875dfb2b3ac6e07cd57b
Rumen <rumenmitov@protonmail.com>
2024-09-19 12:29:26 +0200

    fix: added user_id for ratings

--- src/sql/other.sql
+++ src/sql/other.sql
@@ -34,8 +34,12 @@ CREATE TABLE Ratings (
     rating_id INT PRIMARY KEY AUTO_INCREMENT,
     score INT NOT NULL,
     recipe_id INT,
+    user_id INT,
     FOREIGN KEY (recipe_id)
         REFERENCES Recipes(recipe_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (user_id)
+        REFERENCES Users(user_id)
         ON DELETE CASCADE
 );
 
