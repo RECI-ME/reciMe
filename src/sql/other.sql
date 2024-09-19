8770c4cd74d98d7b399dffc57f21ef86e4fb33ec
Rumen <rumenmitov@protonmail.com>
2024-09-19 12:00:06 +0200

    fix: split up sql init into different files

---
+++ src/sql/other.sql
@@ -0,0 +1,53 @@
+DROP TABLE IF EXISTS 
+    Reviews, 
+    Favorites, 
+    Ratings, 
+    MarkedReviews,
+
+
+CREATE TABLE Reviews (
+    review_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    recipe_id INT,
+    review_text TEXT,
+    FOREIGN KEY (user_id) 
+        REFERENCES Users(user_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (recipe_id) 
+        REFERENCES Recipes(recipe_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE Favorites (
+    favorite_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    recipe_id INT,
+    FOREIGN KEY (user_id) 
+        REFERENCES Users(user_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (recipe_id) 
+        REFERENCES Recipes(recipe_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE Ratings (
+    rating_id INT PRIMARY KEY AUTO_INCREMENT,
+    score INT NOT NULL,
+    recipe_id INT,
+    FOREIGN KEY (recipe_id)
+        REFERENCES Recipes(recipe_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE MarkedReviews (
+    marked_review_id INT PRIMARY KEY AUTO_INCREMENT,
+    review_id INT,
+    user_id INT NOT NULL,
+    FOREIGN KEY (review_id)
+        REFERENCES Reviews(review_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (user_id)
+        REFERENCES Users(user_id)
+        ON DELETE CASCADE
+);
+
---
+++ src/sql/users.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS 
+    Users, 
+    Chefs, 
+    MichelinChefs;
+
+
+CREATE TABLE Users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    since TIMESTAMP NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Chefs (
+    chef_id INT PRIMARY KEY AUTO_INCREMENT,
+    since TIMESTAMP NOT NULL,
+    user_id INT UNIQUE,
+    FOREIGN KEY (user_id) 
+        REFERENCES Users(user_id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE CertifiedChefs (
+    certified_chef_id INT PRIMARY KEY AUTO_INCREMENT,
+    since TIMESTAMP NOT NULL,
+    chef_id INT,
+    FOREIGN KEY (chef_id) 
+        REFERENCES Chefs(chief_id)
+        ON DELETE CASCADE
+);
+
