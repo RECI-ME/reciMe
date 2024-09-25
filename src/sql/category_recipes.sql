465ca6aac237b22767f29a624d737270886a8191
ElyesChouikha <160039506+ElyesChouikha@users.noreply.github.com>
2024-09-25 12:41:55 +0200

    Update category_recipes.sql

--- src/sql/category_recipes.sql
+++ src/sql/category_recipes.sql
@@ -3,5 +3,5 @@ COUNT(r.recipe_id) AS recipe_count,
 GROUP_CONCAT(r.name SEPARATOR '\n') AS recipe_names
 FROM Recipes r
 JOIN Categories c ON r.recipe_id = c.recipe_id
-WHERE c.name = 'Category'
+WHERE c.name = 'Dessert'
 GROUP BY c.name;
