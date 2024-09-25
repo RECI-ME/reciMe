99f1a85ebacdc10045bcf5a5e67177019105b405
ElyesChouikha <160039506+ElyesChouikha@users.noreply.github.com>
2024-09-25 13:54:17 +0200

    Update user_favorites.sql

--- src/sql/user_favorites.sql
+++ src/sql/user_favorites.sql
@@ -1,5 +1,5 @@
 SELECT u.username, 
-GROUP_CONCAT(r.name SEPARATOR '\n') AS favorite_recipes
+GROUP_CONCAT(r.name SEPARATOR ', ') AS favorite_recipes
 FROM Favorites f
 JOIN Users u ON f.user_id = u.user_id
 JOIN Recipes r ON f.recipe_id = r.recipe_id
