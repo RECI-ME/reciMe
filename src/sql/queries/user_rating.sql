a7ba5667544d453cfdfff5c109492b870531c3f3
Rumen <rumenmitov@protonmail.com>
2024-09-26 12:27:51 +0200

    fix #20

--- src/sql/queries/user_rating.sql
+++ src/sql/queries/user_rating.sql
@@ -1,6 +1,5 @@
 -- Show all ratings given by a specific user along with the recipe names
-SELECT u.username, r.name AS recipe_name, ra.score
-FROM Ratings ra
-JOIN Users u ON ra.user_id = u.user_id
-JOIN Recipes r ON ra.recipe_id = r.recipe_id
--- WHERE u.username = 'specific_username';  -- Replace 'specific_username' as needed
+SELECT Users.username, Recipes.name, Ratings.score
+FROM Ratings
+INNER JOIN Users ON Ratings.user_id = Users.user_id
+INNER JOIN Recipes ON Ratings.recipe_id = Recipes.recipe_id
