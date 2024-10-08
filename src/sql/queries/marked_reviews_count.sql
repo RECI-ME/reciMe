4b79ff5f5553c1ced9ede61780b0bd60f391d6ab
Rumen <rumenmitov@protonmail.com>
2024-10-08 09:46:39 +0200

    feat: sample query added

---
+++ src/sql/queries/marked_reviews_count.sql
@@ -0,0 +1,6 @@
+-- Count how many marked reviews each user has created
+SELECT u.user_id, u.username, COUNT(r.marked_reviews.id) AS reviews_count
+FROM Users u
+JOIN MarkedReviews r ON u.user_id = r.user_id
+GROUP BY u.user_id
+ORDER BY reviews_count DESC;
