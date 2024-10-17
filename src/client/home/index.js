0631f59be75723f68186247d01bf4b7391563949
Mahammad <memmedlimehemmed06@gmail.com>
2024-10-17 19:38:17 +0200

    update new log in page, and the like button(the backend is not working)

--- src/client/home/index.js
+++ src/client/home/index.js
@@ -4,14 +4,44 @@ window.onload = function() {
     dom_objs.banner_profile = document.querySelector("div#banner div#profile");
     dom_objs.dashboard_blanket = document.querySelector("div#dashboard_blanket");
     dom_objs.dashboard = document.querySelector("div#dashboard");
+    dom_objs.loginButton = document.querySelector("#loginButton");
 
-    dom_objs.banner_profile.addEventListener("click", function() {
-        dom_objs.dashboard.classList.toggle("hide");
-        dom_objs.dashboard_blanket.classList.toggle("hide");
-    })
+    dom_objs.banner_profile.addEventListener("click", toggleDashboard);
+    dom_objs.dashboard_blanket.addEventListener("click", toggleDashboard);
 
-    dom_objs.dashboard_blanket.addEventListener("click", function() {
-        dom_objs.dashboard.classList.toggle("hide");
-        dom_objs.dashboard_blanket.classList.toggle("hide");
-    })
+    dom_objs.loginButton.addEventListener("mouseover", () => animateButton(true));
+    dom_objs.loginButton.addEventListener("mouseout", () => animateButton(false));
+
+    const likeButtons = document.querySelectorAll('.reactions button');
+
+    likeButtons.forEach((button) => {
+        const likeImage = button.querySelector('img');
+
+        // Only bind event to like button
+        if (likeImage.alt === "Like Icon") {
+            const defaultLikeImage = '../../../assets/like_icon.png';
+            const activeLikeImage = '../../../assets/liked.png';
+
+            button.addEventListener('click', () => {
+                if (likeImage.src.includes('like_icon.png')) {
+                    likeImage.src = activeLikeImage;
+                } else {
+                    likeImage.src = defaultLikeImage;
+                }
+            });
+        }
+    });
+}
+
+function toggleDashboard() {
+    dom_objs.dashboard.classList.toggle("hide");
+    dom_objs.dashboard_blanket.classList.toggle("hide");
+}
+
+function animateButton(isHovering) {
+    if (isHovering) {
+        dom_objs.loginButton.style.transform = "scale(1.1)";
+    } else {
+        dom_objs.loginButton.style.transform = "scale(1)";
+    }
 }
