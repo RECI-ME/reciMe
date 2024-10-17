43329aea5f605985bf3dd028cf272ba5b5bb3e42
Mahammad <memmedlimehemmed06@gmail.com>
2024-10-17 23:07:34 +0200

    fix: added error messages in the login page

--- login/backend/db.php
+++ login/backend/db.php
@@ -9,6 +9,7 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+    die("Connection failed. Please try again later.");
 }
+
 ?>
