94185fb533ed7291e68d1f203fc4c64800e62d74
Mahammad <memmedlimehemmed06@gmail.com>
2024-10-17 19:38:17 +0200

    update new log in page, and the like button(the backend is not working)

---
+++ login/backend/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "";
+$db_name = "";  
+$username = "";
+$password = ""; 
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
