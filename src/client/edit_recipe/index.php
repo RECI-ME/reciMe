8daed7b366a560bc2722a202bec4110f27f50bdb
Rumen <rumenmitov@protonmail.com>
2024-10-18 15:25:12 +0200

    feat: edit recipes

--- src/client/edit_recipe/index.php
+++ src/client/edit_recipe/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>ReciMe | Post Recipe</title>
+        <title>ReciMe | Edit Recipe</title>
         <link rel="icon" type="image/png" href="../../../assets/recimelogo.png">
 
         <link rel="stylesheet" href="../global.css">
@@ -11,7 +11,43 @@
         <form action="../../server/post_recipe.php" method="post">
             <input type="hidden" name="user_id" value="1" />
 
-            <label for="name">Recipe Name</label>
+            <label for="recipe">Recipe</label>
+            <select name="recipe" > 
+                <?php
+                    $env = parse_ini_file("../../../env.ini");
+                    $user_id = 1;
+
+                    if ($env == false) {
+                        echo "server conn error!";
+                    }
+
+                    $conn = new mysqli(
+                        $env["DB_SERVER"], 
+                        $env["DB_USERNAME"], 
+                        $env["DB_PASSWORD"],
+                        $env["DB"]
+                    );
+
+                    if ($conn->connect_error) {
+                        echo "server conn error!";
+                    }
+
+                    $recipes_result = $conn->query("SELECT name FROM Recipes WHERE Recipes.user_id = " . $user_id);
+
+                    if ($recipes_result->num_rows == 0) {
+                        echo "couldn't get user recipes from server!";
+                    }
+
+                    while ($row = $recipes_result->fetch_assoc()) {
+                        echo "<option value='" . $row["name"] . "'>" . $row["name"] . "</option>";
+                    }
+
+
+                    $conn->close();
+                ?>
+            </select>
+
+            <label for="name">Name</label>
             <input name="name" type="text" />
 
             <label for="category">Category</label>
@@ -48,6 +84,7 @@
                     $conn->close();
                 ?>
             </select>
+            
             
             <button type="submit">Post</button>
         </form>
